5cef2e2f275d2888c044c22d4426e490e7042c04
Makoto Goto <mgomao@gmail.com>
2019-11-13 00:56:47 +0900

    11/6 分

---
+++ Assets/Scripts/BundleSprite.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class BundleSprite : MonoBehaviour
+{
+    [SerializeField] AssetReference AssetRefImage;
+
+    UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<Sprite> operation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var sprite = GetComponent<SpriteRenderer> ();
+        if (sprite != null) {
+
+            operation = Addressables.LoadAssetAsync<Sprite> (AssetRefImage);
+            operation.Completed += op => {
+
+                // 読み込み終了時
+                sprite.sprite = op.Result;
+            };
+        }
+    }
+
+    void Update ()
+    {
+
+    }
+}
