49afdf5b79252af96221867359256de5cea6b6f4
Makoto Goto <mgomao@gmail.com>
2019-12-04 17:15:23 +0900

    無駄を削除、関数を修正

--- Server/server.py
+++ Server/server.py
@@ -7,12 +7,12 @@ def hello_world():
     return 'Ready Server!'
 
 @app.route("/log", methods=["POST"])
-def ranking_post():
+def log_post():
     data = request.form['area']
     return  "Ranking POST area=" + data
 
 @app.route("/log", methods=["GET"])
-def history_get():
+def log_get():
     area = request.args.get("area")
     return "Ranking GET area=" + area
 
