330a9b05f59e9727fb08d062cf4f5aba5a61ff20
Makoto Goto <mgomao@gmail.com>
2019-12-11 16:24:14 +0900

    UDP 通信サンプル

---
+++ Assets/Scripts/UDPPlayerSender.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class UDPPlayerSender : MonoBehaviour
+{
+    [SerializeField] UnityEngine.UI.Button btnConnect;
+    [SerializeField] GameObject RefObject;
+
+    public string HostIP = "127.0.0.1";
+    public int HostPort = 6000;
+
+    UdpClient myClient;
+
+    byte[] sendBytes = new byte[32];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnConnect.onClick.AddListener(()=>{
+            // UDP -> Host へ接続
+            myClient = new UdpClient();
+            myClient.Connect(HostIP, HostPort);
+        });
+    }
+
+    void OnApplicationQuit()
+    {
+        if (myClient != null){
+            myClient.Close();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (RefObject == null){
+            // 操作するオブジェクトが決まっていないときは何もしない
+            return;
+        }
+
+        const float SPEED = 1.0f;
+        var pos = RefObject.transform.position;
+        bool isSend = false;
+
+        // プレイヤーの場合：移動操作＆送信
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            pos.x -= SPEED * Time.deltaTime;
+            isSend = true;
+        }
+        if (Input.GetKey(KeyCode.RightArrow)){
+            pos.x += SPEED * Time.deltaTime;
+            isSend = true;
+        }
+        if (Input.GetKey(KeyCode.UpArrow)){
+            pos.z += SPEED * Time.deltaTime;
+            isSend = true;
+        }
+        if (Input.GetKey(KeyCode.DownArrow)){
+            pos.z -= SPEED * Time.deltaTime;
+            isSend = true;
+        }
+
+        RefObject.transform.position = pos;
+
+        if (isSend && myClient != null){
+            // BOXの名前と、ポジションを送信
+            Vec2Byte(RefObject.name, ref pos, sendBytes);
+            myClient.Send(sendBytes, sendBytes.Length);
+        }
+    }
+
+    public void Vec2Byte(string name, ref Vector3 pos, byte[] bytes)
+    {
+        /*
+            バイト配列のフォーッマット
+            byte [16]   Name
+            byte [4]    pox.x
+            byte [4]    pox.y
+            byte [4]    pox.z
+        */
+        byte[] data = System.Text.Encoding.ASCII.GetBytes(name);
+        for (int i=0 ; i<15 && i<data.Length ; ++i){
+            bytes[i] = data[i];
+        }
+        for (int i=data.Length ; i<15 ; ++i){
+            bytes[i] = 0;
+        }
+        bytes[15] = 0;
+
+        byte[] x = System.BitConverter.GetBytes(pos.x);
+        byte[] y = System.BitConverter.GetBytes(pos.y);
+        byte[] z = System.BitConverter.GetBytes(pos.z);
+        for (int i=0 ; i<4 ; ++i){
+            bytes[16+i] = x[i];
+            bytes[20+i] = y[i];
+            bytes[24+i] = z[i];
+        }
+    }
+}
---
+++ Assets/Scripts/UDPReceiver.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class UDPReceiver : MonoBehaviour
+{
+    public int HostPort = 6000;
+
+    UdpClient hostClient;
+
+    byte[] receiveBytes = new byte[32];
+    Vector3 receiveVec3 = new Vector3();
+
+    Thread receiveThread;
+    bool isHostStop = false;
+
+    IPAddress[] adrList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 自分のIPアドレスを取得
+        string hostname = Dns.GetHostName();
+        adrList = Dns.GetHostAddresses(hostname);
+        
+        // Host を起動
+        hostClient = new UdpClient(HostPort);
+        hostClient.Client.ReceiveTimeout = 1000;
+        receiveThread = new Thread(new ThreadStart(ReceiveMain));
+    }
+
+    void OnApplicationQuit()
+    {
+        if (receiveThread != null){
+            isHostStop = true;
+            Thread.Sleep(1);
+        }
+
+        if (hostClient != null){
+            hostClient.Close();
+        }
+    }
+
+    void ReceiveMain()
+    {
+        while (isHostStop == false)
+        {
+            IPEndPoint remoteEP = null;
+            receiveBytes = hostClient.Receive(ref remoteEP);
+
+            // 受信データを取得
+            string refObjName = Byte2Vec(receiveBytes, ref receiveVec3);
+
+            // 参照するオブジェクトを検索
+            var findObj = GameObject.Find(refObjName);
+            if (findObj != null){
+                // 見つかったオブジェクトへ位置を設定
+                findObj.transform.position = receiveVec3;
+            }
+
+            Thread.Sleep(1);
+        }
+        receiveThread = null;
+    }
+
+    string Byte2Vec(byte[] bytes, ref Vector3 pos)
+    {
+        /*
+            バイト配列のフォーッマット
+            byte [16]   Name
+            byte [4]    pox.x
+            byte [4]    pox.y
+            byte [4]    pox.z
+        */
+        name = System.Text.Encoding.ASCII.GetString(bytes);
+
+        pos.x = System.BitConverter.ToSingle(bytes, 16);
+        pos.y = System.BitConverter.ToSingle(bytes, 20);
+        pos.z = System.BitConverter.ToSingle(bytes, 24);
+
+        return name
+    }
+
+    void OnGUI()
+    {
+        GUIStyle myStyle = new GUIStyle(GUI.skin.textArea);
+        myStyle.fontSize = 30;
+
+        int posY = 10;
+        if (adrList != null && adrList.Length > 0){
+            GUI.Label(new Rect(10,posY, 600, 100), "HOST IP : " + adrList[0].ToString(), myStyle);
+            posY += 40;
+        }
+        GUI.Label(new Rect(10, posY, 600, 100), "HOST Port : " + HostPort.ToString(), myStyle);
+        posY += 40;
+    }
+}
